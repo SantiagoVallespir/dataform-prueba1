15ecc84cb504286bf102e043578760e5690ed78c
Santiago Vallespir <santiagovallespir@gmail.com>
2025-10-07 15:36:39 +0000

    modificacioens

---
+++ definitions/sources/sources.js
@@ -0,0 +1,11 @@
+declare({
+  database: "dataform-prueba-1",
+  schema: "world_bank",
+  name: "indicators_data",
+});
+
+declare({
+  database: "dataform-prueba-1",
+  schema: "world_bank",
+  name: "international_education",
+});
